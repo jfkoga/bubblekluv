39942d5df6f700d1451ab3fa6adca53ebc9fae9b
Joan Francesc Konate <joan.francesc.konate@ticxcat.com>
2024-09-08 18:04:21 +0200

    new ewb

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import * as THREE from './node_modules/three/build/three.module.js';
+import * as THREE from './libs/three.module.js';
 
 // Configuración básica de la escena
 const scene = new THREE.Scene();
