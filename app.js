8c98a3d3e17a6eb2f4e9139e1d4c871688b46952
Joan Francesc Konate <joan.francesc.konate@ticxcat.com>
2024-09-10 22:49:30 +0200

    added audio player js

--- app.js
+++ app.js
@@ -162,3 +162,19 @@ window.addEventListener('resize', () => {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
 });
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const playPauseBtn = document.getElementById('playPauseBtn');
+    const audio = document.getElementById('audio');
+    
+    playPauseBtn.addEventListener('click', function() {
+        if (audio.paused) {
+            audio.play();
+            playPauseBtn.textContent = 'Pause';
+        } else {
+            audio.pause();
+            playPauseBtn.textContent = 'Play';
+        }
+    });
+});
