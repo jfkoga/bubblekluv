828af79cb76cac4819cacd19b6f6b63bdac43bb4
Joan Francesc Konate <joan.francesc.konate@ticxcat.com>
2025-03-30 19:19:23 +0200

    restored camera code

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import * as THREE from './libs/three.module.js';
 
-// Crear la escena, cámara y renderer
+// Crear la escena, cámara y renderer bubblekluv
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer({ alpha: true });
