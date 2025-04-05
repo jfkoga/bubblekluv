dae31e0b20f27c120e22986f2ab77fea8f6bba88
Joan Francesc Konate <joan.francesc.konate@ticxcat.com>
2025-04-06 01:34:45 +0200

    arrow image added

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-import * as THREE from './libs/three.module.js';
-
 // Crear la escena, cámara y renderer bubbles
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
