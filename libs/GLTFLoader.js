8b309ea5646ecf9a6298badbd74734dc74c70600
Joan Francesc Konate <joan.francesc.konate@ticxcat.com>
2025-05-17 21:33:03 +0200

    added library

--- libs/GLTFLoader.js
+++ libs/GLTFLoader.js
@@ -65,7 +65,7 @@ import {
 	SRGBColorSpace,
 	InstancedBufferAttribute
 } from './three.module.js';
-import { toTrianglesDrawMode } from '../utils/BufferGeometryUtils.js';
+import { toTrianglesDrawMode } from './libs/BufferGeometryUtils.js';
 
 /**
  * A loader for the glTF 2.0 format.
